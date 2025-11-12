0ece177307711e72667c0a3331753754bbf577b7
张扬 <309220728@qq.com>
2025-11-12 14:56:10 +0800

    删除 main

--- src/main/java/cn/city/parking/config/starter/CommonAutoConfig.java
+++
@@ -1,9 +0,0 @@
-package cn.city.parking.config.starter;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource(factory = YamlPropertySourceFactory.class, value = {"classpath:application-${spring.profiles.active}.yml","classpath:bootstrap-${spring.profiles.active}.yml"})
-public class CommonAutoConfig {
-}
